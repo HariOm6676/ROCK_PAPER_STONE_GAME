a8fb702656de397d7dd0f55346a177ed4f1e7987
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2023-12-07 13:26:59 +0530

    Rock- Scissor - Paper Game

--- resources/scripts/rps.js
+++ resources/scripts/rps.js
@@ -1,6 +1,14 @@
 let userMove = '';
 let computerMove = '';
 let result = '';
+const game = {
+    wins: 0,
+    looses: 0,
+    ties: 0
+};
+function captureUserMove(move) {
+    userMove = move;
+}
 function generateComputerMove() {
     const readNum = Math.random();
     if (readNum < 1 / 3) {
@@ -17,10 +25,36 @@ function evaluateMoves() {
     if (userMove == computerMove) {
         result = 'Tie';
     }
-    else if ((userMove === 'Rock' && computerMove === 'Scissors') || (userMove === 'Paper' && computerMove === 'Rock')) {
+    else if ((userMove === 'Rock' && computerMove === 'Scissors') || (userMove === 'Paper' && computerMove === 'Rock') || (userMove === 'Scissors' && computerMove === 'Paper')) {
         result = 'Win';
     }
     else {
         result = 'Loose';
     }
+}
+function renderGameSummary() {
+    const gamesPlayed = game.wins + game.looses + game.ties;
+    console.log(`userMove ${userMove} computerMove ${computerMove} result: ${result} gamesPlayed: ${gamesPlayed}`);
+
+
+    document.querySelector('#wins').innerHTML = game.wins;
+    document.querySelector('#looses').innerHTML = game.looses;
+    document.querySelector('#ties').innerHTML = game.ties;
+    document.querySelector('#gamesplayed').innerHTML = gamesPlayed;
+    console.log(game);
+
+}
+function updateGameScore() {
+    if (result == 'Win') {
+        game.wins++;
+    }
+    else if (result == 'Tie') {
+        game.ties++;
+    }
+    else {
+        game.looses++;
+    }
+}
+function renderGameHistory() {
+    console.log(`userMove ${userMove} computerMove ${computerMove} result: ${result} gamesPlayed: ${gamesPlayed}`);
 }
